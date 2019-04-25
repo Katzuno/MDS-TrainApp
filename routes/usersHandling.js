816781c1d24df329d080aee347ad07a6da673f52
Katzuno <erikhenning98@hotmail.com>
2019-04-26 00:52:47 +0300

    Main endpoints for user interactions

--- routes/usersHandling.js
+++ routes/usersHandling.js
@@ -34,6 +34,67 @@ router.get('/getUsers', function(req, res) {
     console.log('Query still executing...')
 });
 
+router.get('/getUserId', function(req, res) {
+    res.header('Content-Type', 'application/json');
+
+    if (req.query.hasOwnProperty('username'))
+    {
+        console.info('Username=', req.query.username);
+        query = db.Operation("SELECT id FROM " + process.env.USERS_TABLE + " WHERE username = '" + req.query.username + "' LIMIT 1");
+        query.then(function(result) {
+                //console.log("[RESULT] ", result);
+                //return result;
+                res.send(result[0]);
+
+            },
+            function(err) {
+                //console.error(err);
+                res.send(err);
+            }
+        );
+
+    }
+    else
+    {
+        response = {"Error":"Username not specified"};
+        res.send(response);
+    }
+    console.log('Query still executing...')
+});
+
+router.get('/getUsers', function(req, res) {
+    res.header('Content-Type', 'application/json');
+
+    if (req.query.hasOwnProperty('id'))
+    {
+        console.info('Id=', req.query.id);
+        query = db.Operation("SELECT * FROM " + process.env.USERS_TABLE + " WHERE id = " + req.query.id + " LIMIT 1");
+    }
+    else
+    {
+        query = db.Operation("SELECT * FROM " + + process.env.USERS_TABLE);
+        console.info("Nu exista id, se vor selecta toti");
+    }
+
+
+    query.then(function(result) {
+            //console.log("[RESULT] ", result);
+            //return result;
+            res.send(result);
+
+        },
+        function(err) {
+            //console.error(err);
+            res.send(err);
+        }
+    );
+
+    console.log('Query still executing...')
+});
+
+
+
+
 router.post('/addUser', function(req, res) {
     res.header('Content-Type', 'application/json');
 
@@ -71,4 +132,49 @@ router.post('/addUser', function(req, res) {
 
 });
 
+
+
+router.post('/validateUser', function(req, res) {
+    res.header('Content-Type', 'application/json');
+
+    let query, response;
+
+    var username = req.body.username;
+    var pass = req.body.password;
+
+    sql_string = "SELECT id FROM " + process.env.USERS_TABLE + " WHERE username = '" + username + "' AND password = '" + pass + "' LIMIT 1";
+    query = db.Operation(sql_string);
+
+    query.catch(function(err) {
+        //console.error(err);
+        response = {"Status": 0, "Desc": err}
+        res.send(response);
+    });
+
+    query.then(function(result) {
+            //console.log("[RESULT] ", result);
+            //return result;
+            if (result.length > 0)
+            {
+                response = {"Status": 1, "Desc": "User authenticated succesfully"};
+            }
+            else
+            {
+                response = {"Status": 0, "Desc": "Username or password is not correct"};
+            }
+
+            res.send(response);
+
+        },
+        function(err) {
+            //console.error(err);
+            response = {"Status": 0, "Desc": err}
+            res.send(response);
+        }
+
+    );
+
+    console.log('Query still executing...')
+
+});
 module.exports = router;
